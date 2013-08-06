cb671e613a6c8b10bd560167e86e236a4a2f0cf6
Kevin Gessner <kevin@fogcreek.com>
2013-08-06 18:20:19 +0400

    first pass

---
+++ cssparsec/Main.fs
@@ -0,0 +1,40 @@
+﻿module cssparsec.Main
+
+open FParsec
+open System
+
+type SpecifyingSelector =
+    | IdSelector of string
+    | ClassSelector of string
+
+type TagSelector =
+    | TagSelector of string
+
+type SingleSelector =
+    | SpecifyingOnlySelector of SpecifyingSelector list
+    | SpecifiedTagSelector of TagSelector * (SpecifyingSelector list)
+
+type Selector =
+    | DescendantSelector of SingleSelector list
+
+let parseIdentifier : Parser<string, unit> =
+    many1Satisfy2 isAsciiLower (fun c -> isAsciiLetter c || isDigit c || c = '_' || c = '-')
+
+let parseIdSelector : Parser<SpecifyingSelector, unit> =
+    (pchar '#' >>? parseIdentifier) |>> IdSelector
+
+let parseClassSelector : Parser<SpecifyingSelector, unit> =
+    (pchar '.' >>? parseIdentifier) |>> ClassSelector
+
+let parseSpecifyingSelectors : Parser<SpecifyingSelector list, unit> =
+    many (choice [parseIdSelector; parseClassSelector])
+
+let parseTagSelector : Parser<TagSelector, unit> =
+    (many1Satisfy isAsciiLower) |>> TagSelector
+
+let parseSingleSelector : Parser<SingleSelector, unit> =
+    attempt (tuple2 parseTagSelector parseSpecifyingSelectors) |>> SpecifiedTagSelector <|>
+        (parseSpecifyingSelectors |>> SpecifyingOnlySelector)
+
+let parseSelector : Parser<Selector, unit> =
+    sepBy1 parseSingleSelector spaces1 |>> DescendantSelector
---
+++ cssparsec/Tests.fs
@@ -0,0 +1,51 @@
+﻿module cssparsec.Tests
+
+open FParsec
+open System
+open NUnit.Framework
+
+open cssparsec.Main
+
+let go parser text =
+    match run parser text with
+        | Success (r, _, _) -> r
+        | Failure (m, _, _) -> raise (Exception m)
+
+[<Test>]
+let testIdSelector() =
+    Assert.AreEqual(IdSelector "abc", go parseIdSelector "#abc")
+
+[<Test>]
+let testClassSelector() =
+    Assert.AreEqual(ClassSelector "class", go parseClassSelector ".class")
+
+[<Test>]
+let testTagSelector() =
+    Assert.AreEqual(TagSelector "span", go parseTagSelector "span")
+
+[<Test>]
+let testSelectorForSpecifiers() =
+    Assert.AreEqual(SpecifyingOnlySelector [IdSelector "id"], go parseSingleSelector "#id")
+    Assert.AreEqual(SpecifyingOnlySelector [ClassSelector "class"], go parseSingleSelector ".class")
+    Assert.AreEqual(SpecifyingOnlySelector [ClassSelector "abc"; ClassSelector "defg"], go parseSingleSelector ".abc.defg")
+
+[<Test>]
+let testSelectorForTag() =
+    Assert.AreEqual(SpecifiedTagSelector (TagSelector "span", []), go parseSingleSelector "span")
+
+[<Test>]
+let testSelectorForSpecifiedTags() =
+    Assert.AreEqual(SpecifiedTagSelector (TagSelector "span", [IdSelector "foo"]), go parseSingleSelector "span#foo")
+    Assert.AreEqual(SpecifiedTagSelector (TagSelector "span", [ClassSelector "foo"]), go parseSingleSelector "span.foo")
+    Assert.AreEqual(SpecifiedTagSelector (TagSelector "span", [ClassSelector "foo"; ClassSelector "bar"]), go parseSingleSelector "span.foo.bar")
+    Assert.AreEqual(SpecifiedTagSelector (TagSelector "span", [IdSelector "foo"; ClassSelector "bar"]), go parseSingleSelector "span#foo.bar")
+
+[<Test>]
+let testDescendantSelectors() =
+    Assert.AreEqual(DescendantSelector [SpecifiedTagSelector (TagSelector "span", []);
+                     SpecifiedTagSelector (TagSelector "div", [ClassSelector "foo"]);
+                     SpecifyingOnlySelector [IdSelector "bar"]],
+                    go parseSelector "span div.foo #bar")
+
+[<EntryPoint>]
+let main args = 0
